dfbbf4ec0f310e94d539797245e64fdbe7d69138
JohnS333 <johnsimson101@outlook.com>
2025-03-13 10:59:01 -0400

    made page eligible for localhost hosting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@
 
 // console.log('ASAP TEXT');
 
+const express = require('express');
+const { readFile } = require('fs').promises; // Destructure readFile from fs module
 
-const myModule = require('./my-module');
+const app = express();
 
-console.log(myModule);
+// Serve static files from the '.' directory, aka the one we are in, src
+app.use(express.static('.'));      
+
+app.get('/', async (request, response) => { 
+    response.send(
+    await readFile('./index.html', 'utf8', (err, html) => {
+        if (err) {
+            response.status(500).send('Sorry, out of order');
+        }
+        response.send(html);
+    })
+)
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
